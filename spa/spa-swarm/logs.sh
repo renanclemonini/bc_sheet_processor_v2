4158af22654cb14a6d25bc22e2e1944814672f64
Renan Clemonini <rclemon93@gmail.com>
2026-08-13 20:47:53 -0300

    fix: Testing values passed in the script to show history greater than 5 lines.

--- spa/spa-swarm/logs.sh
+++ spa/spa-swarm/logs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cd "$(dirname "$0")/../.."
+#cd "$(dirname "$0")/../.."
+
+TAIL="${1:-5}"
 
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 echo -e "${GREEN} Mostrando logs (Swarm)...${NC}\n"
 
-docker service logs -f bc_sheets_processor_swarm_sheet-processor --tail=5
+docker service logs -f bc_sheets_processor_swarm_sheet-processor --tail="$TAIL"
